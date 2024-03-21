fa5d8aac0b9dfcae973a060fadf9627a66049cea
nmanyange <140177684+nmanyange@users.noreply.github.com>
2024-03-21 15:57:48 +0200

    Add files via upload

---
+++ 1-index.js
@@ -0,0 +1,20 @@
+/*  Create 2 funtions, add3points() and remove1Point and have them add or remove points to or from myPoints variable.
+Call the functions until they log out 10.*/
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+    console.log(myPoints)
+}
+
+function remove1Point() {
+    myPoints -= 1
+    console.log(myPoints)
+}
+
+add3Points()
+remove1Point()
+add3Points()
+remove1Point()
+add3Points()
